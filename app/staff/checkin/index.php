8346173967d655d53d055b7b11e0d4e6e7af1030
UncleMilobear <107782416+NestumMiloBertaburan@users.noreply.github.com>
2024-01-13 11:44:00 +0800

    update

--- app/staff/checkin/index.php
+++ app/staff/checkin/index.php
@@ -48,7 +48,8 @@ require "../template/adminpart.php";
                     <?php
                     $sql = "SELECT booking.*, room.*, guest.* FROM booking
                     JOIN room ON booking.roomID = room.roomID
-                    JOIN guest ON booking.guestID = guest.guestID";
+                    JOIN guest ON booking.guestID = guest.guestID
+                    WHERE booking.status = 'confirmed'";
 
                     $result = mysqli_query($conn, $sql);
                     
@@ -61,10 +62,10 @@ require "../template/adminpart.php";
                         $numrow = 1;
                         while ($row = mysqli_fetch_assoc($result)) {
                             echo "<tr>";
-                            echo "<td>" . $numrow . "</td><td>" . $row["firstName"] . " " . $row["lastName"] . "</td><td>" . $row["roomID"] . "</td><td>" . $row["check_in"] .
+                            echo "<td>" . $numrow . "</td><td>" . $row["firstName"] . " " . $row["lastName"] . "</td><td>" . $row["roomNo"] . "</td><td>" . $row["check_in"] .
                                 "</td><td>" . $row["check_out"] . "</td>";
-                            echo '<td> <a href="checkin.php?id=' . $row["bookID"] . '">Check-In</a>&nbsp;|&nbsp;';
-                            echo '<a href="cancel.php?id=' . $row["bookID"] . '">Cancel</a> </td>';
+                            echo '<td> <a href="checkin.php?book=' . $row["bookID"] . '">Check-In</a>&nbsp;|&nbsp;';
+                            echo '<a href="cancel.php?book=' . $row["bookID"] . '">Cancel</a> </td>';
                             echo "</tr>" . "\n\t\t";
                             $numrow++;
                         }
